94c7be57da739dfd90ecda15b95cc19d8d8b4614
azeemparvez-miit <21it21.azeem@measiit.edu.in>
2022-01-28 18:07:53 +0530

    2. Write a program to print default values of all primitive types.

---
+++ Assignment1/q2/PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+package q2;
+//2. Write a program to print default values of all primitive types. 
+public class PrimitiveDataTypes {
+	   static boolean val1;
+	   static double val2;
+	   static float val3;
+	   static int val4;
+	   static long val5;
+	   static String val6;
+		   public static void main(String[] args) {
+		      System.out.println("-----Default values of all primitive types-----");
+		      System.out.println("Boolean : " + val1);
+		      System.out.println("Double : " + val2);
+		      System.out.println("Float : " + val3);
+		      System.out.println("Int : " + val4);
+		      System.out.println("Long : " + val5);
+		      System.out.println("String : " + val6);
+
+	}
+
+}

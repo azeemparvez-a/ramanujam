687c3172749c554ce56074b1b4dd0ede666c362f
azeemparvez-miit <21it21.azeem@measiit.edu.in>
2022-01-28 18:58:14 +0530

    4. Write a program to find weather a number is Prime or not

---
+++ Assignment1/q4/PrimeNumber.java
@@ -0,0 +1,24 @@
+package q4;
+
+//4. Write a program to find weather a number is Prime or not 
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int num = 12;
+	    boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      
+	    	
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+}
+}

74fc6d3e2658c12895ddc549863320122fcb3c89
azeemparvez-miit <21it21.azeem@measiit.edu.in>
2022-01-30 14:01:03 +0530

    7. Sort an array element in ascending or descending order

---
+++ Assignment1/q7/SortArray.java
@@ -0,0 +1,16 @@
+package q7;
+
+//7. Write a program to sort an array element in ascending or descending order
+
+import java.util.*;
+public class SortArray {
+
+	public static void main(String[] args) {
+        Integer array[] = { 1, 2, 3, 4, 5 };
+  
+        // Sorting the array in descending order using Arrays.sort() ||[import java.util.*;]
+        Arrays.sort(array, Collections.reverseOrder());
+  
+        System.out.println(Arrays.toString(array)); 
+	}
+}

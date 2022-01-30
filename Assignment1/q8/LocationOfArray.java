8e830853ad53486a3d337fffc4fa53f160e4a6b4
azeemparvez-miit <21it21.azeem@measiit.edu.in>
2022-01-30 14:17:00 +0530

    8. Write a program to find the location of element in the Array.

---
+++ Assignment1/q8/LocationOfArray.java
@@ -0,0 +1,11 @@
+package q8;
+//8. Write a program to find the location of element in the Array.
+public class LocationOfArray {
+
+	public static void main(String[] args) {
+		char[] list = {'m','a','c','p','e', 'y'};
+		// Create e new String from array of chars and then do an indeoxOf("e") on that String:
+		System.out.println(new String(list).indexOf("e")); 
+	}
+
+}
